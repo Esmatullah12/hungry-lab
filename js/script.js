43757d309aa06d834995205e90d87ec5c2bcdbf8
Esmatullah <esmatqadeemi12@gmail.com>
2023-09-22 13:19:53 +0100

    give style to the navbar and hero section images

--- js/script.js
+++ js/script.js
@@ -27,6 +27,31 @@ function getDirection() {
     return direction;
 }
 
+const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".hero-images", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+    });
+
 productImages.forEach(productImage => {
     productImage.addEventListener('click', (event) =>{
         popupImage.src = event.target.src
